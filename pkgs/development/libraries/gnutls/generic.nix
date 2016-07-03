f8aca23f7d5f496ff1a56b617c05012992135fb6
Michiel Leenaars <ml.software@leenaa.rs>
2016-07-03 15:12:45 +0000

    gnutls: temporarily disable checks

--- pkgs/development/libraries/gnutls/generic.nix
+++ pkgs/development/libraries/gnutls/generic.nix
@@ -45,7 +45,8 @@ stdenv.mkDerivation {
 
   # XXX: Gnulib's `test-select' fails on FreeBSD:
   # http://hydra.nixos.org/build/2962084/nixlog/1/raw .
-  doCheck = (!stdenv.isFreeBSD && !stdenv.isDarwin);
+  doCheck = (!stdenv.isFreeBSD && !stdenv.isDarwin && !stdenv.isLinux);
+
 
   # Fixup broken libtool and pkgconfig files
   preFixup = lib.optionalString (!stdenv.isDarwin) ''
